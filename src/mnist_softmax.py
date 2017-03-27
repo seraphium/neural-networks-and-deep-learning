56c8000304db588db4afcd7b00cb52cc6a20726a
Jackie Zhang <viperking@163.com>
2017-03-27 23:57:28 +0800

    chang README implement basic tf code

--- src/mnist_softmax.py
+++ src/mnist_softmax.py
@@ -3,4 +3,14 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
 
-print mnist.train;
+x = tf.placeholder(tf.float32, [None, 784])
+
+w = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros(10))
+
+#output
+y = tf.nn.softmax(tf.matmul(x, w) + b)
+
+#correct answer
+y_ = tf.placeholder(tf.float32, [None, 10])
+
