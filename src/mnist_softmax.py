6f7b22739e846388f91d626c2265c807b1323c78
seraphium <viperking@163.com>
2017-03-29 10:16:05 +0800

    finished basic tf tutorial code

--- src/mnist_softmax.py
+++ src/mnist_softmax.py
@@ -1,16 +1,34 @@
 import tensorflow as tf
+import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
 
+b = tf.Variable(tf.zeros(10))
 x = tf.placeholder(tf.float32, [None, 784])
 
 w = tf.Variable(tf.zeros([784, 10]))
-b = tf.Variable(tf.zeros(10))
 
 #output
 y = tf.nn.softmax(tf.matmul(x, w) + b)
 
 #correct answer
 y_ = tf.placeholder(tf.float32, [None, 10])
+cost = y_ * tf.log(y)
+sumTrainingCost = -tf.reduce_sum(cost, reduction_indices=[1])
+cross_entropy = tf.reduce_mean(sumTrainingCost)
 
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+
+epoch = 1000
+for _ in range(epoch):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={x:batch_xs, y_:batch_ys})
+   # print("training accuracy:",sess.run(accuracy, feed_dict={x: mnist.train.images, y_: mnist.train.labels}))
+
+print("test accuracy:", sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
