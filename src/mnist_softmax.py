49ed76b67cccffce40e0c7f3f1be6b2b38dd71c0
seraphium <viperking@163.com>
2017-03-29 18:06:54 +0800

    finished basic tf tutorial code

--- src/mnist_softmax.py
+++ src/mnist_softmax.py
@@ -14,9 +14,17 @@ y = tf.nn.softmax(tf.matmul(x, w) + b)
 
 #correct answer
 y_ = tf.placeholder(tf.float32, [None, 10])
+'''
 cost = y_ * tf.log(y)
 sumTrainingCost = -tf.reduce_sum(cost, reduction_indices=[1])
 cross_entropy = tf.reduce_mean(sumTrainingCost)
+'''
+# Regularization parameter
+lbda = tf.constant(0.001)
+
+cross_entropy = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y)) + lbda * tf.nn.l2_loss(w)
+    #tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
 
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
@@ -26,9 +34,11 @@ sess = tf.InteractiveSession()
 tf.global_variables_initializer().run()
 
 epoch = 1000
-for _ in range(epoch):
+for index in range(epoch):
     batch_xs, batch_ys = mnist.train.next_batch(100)
     sess.run(train_step, feed_dict={x:batch_xs, y_:batch_ys})
-   # print("training accuracy:",sess.run(accuracy, feed_dict={x: mnist.train.images, y_: mnist.train.labels}))
-
-print("test accuracy:", sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+    if index % 100 == 0 :
+        print("epoch:",  index)
+        print("training accuracy:",sess.run(accuracy, feed_dict={x: mnist.train.images, y_: mnist.train.labels}))
+        print("test accuracy:", sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+#print("test accuracy:", sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
