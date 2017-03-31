c76662bbc12f978cbc6ee8876a4a1efd18adf9c6
seraphium <viperking@163.com>
2017-03-31 11:24:19 +0800

    added monitoring on iris model

--- src/iris.py
+++ src/iris.py
@@ -5,6 +5,8 @@ from __future__ import print_function
 import tensorflow as tf
 import numpy as np
 
+tf.logging.set_verbosity(tf.logging.INFO)
+
 # Data sets
 IRIS_TRAINING = "iris_training.csv"
 IRIS_TEST = "iris_test.csv"
@@ -22,16 +24,46 @@ test_set = tf.contrib.learn.datasets.base.load_csv_with_header(
 # Specify that all features have real-value data
 feature_columns = [tf.contrib.layers.real_valued_column("", dimension=4)]
 
+validation_metrics = {
+    "accuracy":
+        tf.contrib.learn.MetricSpec(
+            metric_fn=tf.contrib.metrics.streaming_accuracy,
+            prediction_key=tf.contrib.learn.PredictionKey.
+            CLASSES),
+    "precision":
+        tf.contrib.learn.MetricSpec(
+            metric_fn=tf.contrib.metrics.streaming_precision,
+            prediction_key=tf.contrib.learn.PredictionKey.
+            CLASSES),
+    "recall":
+        tf.contrib.learn.MetricSpec(
+            metric_fn=tf.contrib.metrics.streaming_recall,
+            prediction_key=tf.contrib.learn.PredictionKey.
+            CLASSES)
+}
+
+# create validation monitor
+validation_monitor = tf.contrib.learn.monitors.ValidationMonitor(
+    test_set.data,
+    test_set.target,
+    every_n_steps=50,
+    metrics=validation_metrics,
+    early_stopping_metric="loss",
+    early_stopping_metric_minimize=True,
+    early_stopping_rounds=200)
+
 # Build 3 layer DNN with 10, 20, 10 units respectively.
 classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
                                             hidden_units=[10, 20, 10],
                                             n_classes=3,
-                                            model_dir="/tmp/iris_model")
+                                            model_dir="/tmp/tensorflow/iris_model",
+                                            config=tf.contrib.learn.RunConfig(save_checkpoints_secs = 1))
 
 # Fit model.
 classifier.fit(x=training_set.data,
                y=training_set.target,
-               steps=2000)
+               steps=2000,
+               monitors=[validation_monitor])
 
 # Evaluate accuracy.
 accuracy_score = classifier.evaluate(x=test_set.data,
