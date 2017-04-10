734a9f4fe7f14f907c74042c5c73d9ad37bdec5f
Jackie Zhang <viperking@163.com>
2017-04-11 00:04:18 +0800

    changed to keras lib

--- src/captcha_train.py
+++ src/captcha_train.py
@@ -8,6 +8,12 @@ from PIL import Image
 import numpy as np
 import tensorflow as tf
 
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import optimizers
+
+
 text, image = gen_captcha_text_and_image()
 print("验证码图像channel:", image.shape)  # (60, 160, 3)
 # 图像大小
@@ -20,10 +26,8 @@ print("验证码文本最长字符数", MAX_CAPTCHA)  # 验证码最长4字符; 
 # 把彩色图像转为灰度图像（色彩对识别验证码没有什么用）
 def convert2gray(img):
     if len(img.shape) > 2:
-        gray = np.mean(img, -1)
-        # 上面的转法较快，正规转法如下
-        # r, g, b = img[:,:,0], img[:,:,1], img[:,:,2]
-        # gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+        gray = np.zeros(shape=[IMAGE_HEIGHT,IMAGE_WIDTH, 1])
+        gray[:, :, 0] = np.mean(image, -1)
         return gray
     else:
         return img
@@ -99,7 +103,7 @@ print(text)  # SFd5
 
 # 生成一个训练batch
 def get_next_batch(batch_size=128):
-    batch_x = np.zeros([batch_size, IMAGE_HEIGHT * IMAGE_WIDTH])
+    batch_x = np.zeros([batch_size, IMAGE_HEIGHT, IMAGE_WIDTH, 1])
     batch_y = np.zeros([batch_size, MAX_CAPTCHA * CHAR_SET_LEN])
 
     # 有时生成图像大小不是(60, 160, 3)
@@ -111,114 +115,58 @@ def get_next_batch(batch_size=128):
 
     for i in range(batch_size):
         text, image = wrap_gen_captcha_text_and_image()
-        image = convert2gray(image)
-
-        batch_x[i, :] = image.flatten() / 255  # (image.flatten()-128)/128  mean为0
+        gray = convert2gray(image)
+        batch_x[i, :] = gray
         batch_y[i, :] = text2vec(text)
 
     return batch_x, batch_y
 
 
-####################################################################
-
-X = tf.placeholder(tf.float32, [None, IMAGE_HEIGHT * IMAGE_WIDTH])
-Y = tf.placeholder(tf.float32, [None, MAX_CAPTCHA * CHAR_SET_LEN])
-keep_prob = tf.placeholder(tf.float32)  # dropout
-
 # 定义CNN
 def crack_captcha_cnn():
-    x = tf.reshape(X, shape=[-1, IMAGE_HEIGHT, IMAGE_WIDTH, 1])
 
-    # w_c1_alpha = np.sqrt(2.0/(IMAGE_HEIGHT*IMAGE_WIDTH)) #
-    # w_c2_alpha = np.sqrt(2.0/(3*3*32))
-    # w_c3_alpha = np.sqrt(2.0/(3*3*64))
-    # w_d1_alpha = np.sqrt(2.0/(8*32*64))
-    # out_alpha = np.sqrt(2.0/1024)
+    model = Sequential()
+    # input: 100x100 images with 3 channels -> (100, 100, 3) tensors.
+    # this applies 32 convolution filters of size 3x3 each.
+    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(IMAGE_HEIGHT, IMAGE_WIDTH, 1)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
 
-    # 3 conv layer
-    w_c1 = tf.Variable(tf.truncated_normal([3, 3, 1, 32], stddev=0.1))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
 
-    b_c1 = tf.Variable(tf.truncated_normal([32]))
-    conv1 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(x, w_c1, strides=[1, 1, 1, 1], padding='SAME'), b_c1))
-    conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-    conv1 = tf.nn.dropout(conv1, keep_prob)
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
 
-    w_c2 = tf.Variable(tf.truncated_normal([3, 3, 32, 64], stddev=0.1))
-    b_c2 = tf.Variable(tf.truncated_normal([64]))
-    conv2 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(conv1, w_c2, strides=[1, 1, 1, 1], padding='SAME'), b_c2))
-    conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-    conv2 = tf.nn.dropout(conv2, keep_prob)
+    model.add(Flatten())
+    model.add(Dense(MAX_CAPTCHA * CHAR_SET_LEN, activation='relu'))
 
-    w_c3 = tf.Variable(tf.truncated_normal([3, 3, 64, 64], stddev=0.1))
-    b_c3 = tf.Variable(tf.truncated_normal([64]))
-    conv3 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(conv2, w_c3, strides=[1, 1, 1, 1], padding='SAME'), b_c3))
-    conv3 = tf.nn.max_pool(conv3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-    conv3 = tf.nn.dropout(conv3, keep_prob)
-
-    # Fully connected layer
-    w_d = tf.Variable(tf.truncated_normal([8 * 32 * 40, 1024], stddev=0.1))
-    b_d = tf.Variable(tf.truncated_normal([1024]))
-    dense = tf.reshape(conv3, [-1, w_d.get_shape().as_list()[0]])
-    dense = tf.nn.relu(tf.add(tf.matmul(dense, w_d), b_d))
-    dense = tf.nn.dropout(dense, keep_prob)
-
-    w_out = tf.Variable(tf.truncated_normal([1024, MAX_CAPTCHA * CHAR_SET_LEN], stddev=0.1))
-    b_out = tf.Variable(tf.truncated_normal([MAX_CAPTCHA * CHAR_SET_LEN]))
-    out = tf.add(tf.matmul(dense, w_out), b_out)
-    # out = tf.nn.softmax(out)
-    return out
+    return model
 
 
 path = ""
 
 # 训练
-output = crack_captcha_cnn()
+model = crack_captcha_cnn()
 # loss
-# loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(output, Y))
-loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=output, labels=Y))
-tf.summary.scalar('loss', loss)
+optimizer = optimizers.SGD(lr=0.1, momentum=0.1, decay=0.0, nesterov=False)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['mae', 'acc'])
 
-# 最后一层用来分类的softmax和sigmoid有什么不同？
-# optimizer 为了加快训练 learning_rate应该开始大，然后慢慢衰
-optimizer = tf.train.AdamOptimizer(learning_rate=0.0003).minimize(loss)
-
-predict = tf.reshape(output, [-1, MAX_CAPTCHA, CHAR_SET_LEN])
+'''
+predict = tf.reshape(model, [-1, MAX_CAPTCHA, CHAR_SET_LEN])
 max_idx_p = tf.argmax(predict, 2)
 max_idx_l = tf.argmax(tf.reshape(Y, [-1, MAX_CAPTCHA, CHAR_SET_LEN]), 2)
 correct_pred = tf.equal(max_idx_p, max_idx_l)
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-tf.summary.scalar('accuracy', accuracy)
+
+'''
 
 
 def train():
-    saver = tf.train.Saver()
-    with tf.Session() as sess:
-
-        merged = tf.summary.merge_all()
-        sess.run(tf.global_variables_initializer())
-        train_writer = tf.summary.FileWriter('/tmp/tensorflow/captcha/train', sess.graph)
-
-        step = 0
-        while True:
-            batch_x, batch_y = get_next_batch(64)
-            summary, train_step, loss_ = sess.run([merged, optimizer, loss], feed_dict={X: batch_x, Y: batch_y, keep_prob: 0.75})
-            print("step:",step, "loss =",loss_)
-            train_writer.add_summary(summary, step)        # 每100 step计算一次准确率
-            if (step % 10 == 0) and (step != 0):
-                batch_x_test, batch_y_test = get_next_batch(100)
-                summary, acc = sess.run([merged, accuracy], feed_dict={X: batch_x_test, Y: batch_y_test, keep_prob: 1.})
-                print(step, "accuracy =",acc)
-                train_writer.add_summary(summary, step)
-
-                # 如果准确率大于50%,保存模型,完成训练
-                if acc > 0.5:
-                    savepath = saver.save(sess, "/tmp/tensorflow/captcha/crack_capcha.ckpt")
-                    print("model saved to :", savepath)
-                    break
-
-            step += 1
-
-    train_writer.close()
+    batch_x, batch_y = get_next_batch(1000)
+    model.fit(batch_x, batch_y, batch_size=20, epochs=1000)
 
 
 train()
@@ -246,5 +194,5 @@ def test():
             predict_text = text_list[0].tolist()
             print("正确: {}  预测: {}".format(text, predict_text))
 
-test()
+#test()
 
